ef232996cbd879e7433a7fdf2e9e1159fd5b1019
antonikurniawan <antonikrnwn@gmail.com>
2021-05-23 15:10:18 +0700

    only create UI

---
+++ app/src/main/java/com/dicoding/watcherapplication/phone/PhoneActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.watcherapplication.phone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.watcherapplication.R
+
+class PhoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone)
+    }
+}
