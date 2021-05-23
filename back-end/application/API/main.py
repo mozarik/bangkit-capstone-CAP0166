27fd8f7bd50452c78e7a4fce3c34b753f87253e1
fachryfathurahman <fachry123fathurahman@gmail.com>
2021-05-23 23:03:30 +0700

    testing upload file

--- back-end/application/API/main.py
+++ back-end/application/API/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
-
+from google.cloud import storage
 app = FastAPI()
 
-
+CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 class Profile(BaseModel):
     name: str
     status: Optional[str] = None
@@ -23,3 +25,22 @@ def read_root():
 @app.post("/profile/create/")
 def create_profile(profile: Profile):
     return profile
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob(file.filename)
+
+    blob.upload_from_string(
+        file.read(),
+        content_type=file.content_type
+    )
+    # The public URL can be used to directly access the uploaded file via HTTP.
+    return blob.public_url
