41af153aa551adef1a146c3eaff9fc225add3dd0
antonikurniawan <antonikrnwn@gmail.com>
2021-05-24 20:50:21 +0700

    update core thingies, and student list, button upload and change image still unavailable function

--- app/src/main/java/com/dicoding/watcherapplication/core/utils/DataDummy.kt
+++ app/src/main/java/com/dicoding/watcherapplication/core/utils/DataDummy.kt
@@ -6,135 +6,58 @@ object DataDummy {
 
     fun generateStudentDummy(): List<Student> {
         val student = ArrayList<Student>()
-        student.add(Student("Tokino Sora"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/4/4b/Tokino_Sora_-_Profile_Picture.jpg/revision/latest?cb=20201229202429"))
+        student.add(
+            Student("Student 1",
+            "https://crestedcranesolutions.com/wp-content/uploads/2013/07/facebook-profile-picture-no-pic-avatar.jpg")
+        )
 
-        student.add(Student("Roboco"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/8/8e/Roboco_-_Profile_Picture.jpg/revision/latest?cb=20201212073048"))
+        student.add(
+            Student("Student 2",
+            "https://dprd.lumajangkab.go.id/uploads/foto_anggota/foto_anggota_200206041459.jpeg")
+        )
 
-        student.add(Student("Sakura Miko"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/5/54/Sakura_Miko_-_Profile_Picture.jpg/revision/latest?cb=20201212075631"))
+        student.add(
+            Student("Student 3",
+                "https://crestedcranesolutions.com/wp-content/uploads/2013/07/facebook-profile-picture-no-pic-avatar.jpg")
+        )
 
-        student.add(Student("Hoshimachi Suisei"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/6/61/Hoshimachi_Suisei_-_Profile_Picture.png/revision/latest?cb=20200825071435"))
+        student.add(
+            Student("Student 4",
+                "https://dprd.lumajangkab.go.id/uploads/foto_anggota/foto_anggota_200206041459.jpeg")
+        )
 
-        student.add(Student("AZKi"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/b/bd/AZKi_-_Profile_Picture.jpg/revision/latest?cb=20210513090727"))
+        student.add(
+            Student("Student 5",
+                "https://crestedcranesolutions.com/wp-content/uploads/2013/07/facebook-profile-picture-no-pic-avatar.jpg")
+        )
 
-        student.add(Student("Yozora Mei"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/3/3c/Yozora_Mel_-_Profile_Picture.jpg/revision/latest?cb=20210510034131"))
+        student.add(
+            Student("Student 6",
+                "https://dprd.lumajangkab.go.id/uploads/foto_anggota/foto_anggota_200206041459.jpeg")
+        )
 
-        student.add(Student("Shirakami Fubuki"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/9/9d/Shirakami_Fubuki_-_Profile_Picture.jpg/revision/latest?cb=20210510034059"))
+        student.add(
+            Student("Student 7",
+                "https://crestedcranesolutions.com/wp-content/uploads/2013/07/facebook-profile-picture-no-pic-avatar.jpg")
+        )
 
-        student.add(Student("Natsuiro Matsuri"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/0/04/Natsuiro_Matsuri_-_Profile_Picture.jpg/revision/latest?cb=20210510032157"))
+        student.add(
+            Student("Student 8",
+                "https://dprd.lumajangkab.go.id/uploads/foto_anggota/foto_anggota_200206041459.jpeg")
+        )
 
-        student.add(Student("Aki Rosenthal"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/4/45/Aki_Rosenthal_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/400?cb=20210510031709"))
+        student.add(
+            Student("Student 9",
+                "https://crestedcranesolutions.com/wp-content/uploads/2013/07/facebook-profile-picture-no-pic-avatar.jpg")
+        )
 
-        student.add(Student("Akai Haato"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/b/b7/Akai_Haato_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/400?cb=20210510032920"))
-
-        student.add(Student("Minato Aqua"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/5/5a/Minato_Aqua_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20200516143158"))
-
-        student.add(Student("Murasaki Shion"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/4/49/Murasaki_Shion_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20200516143458"))
-
-        student.add(Student("Nakiri Ayame"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/1/12/Nakiri_Ayame_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20200516143320"))
-
-        student.add(Student("Yuzuki Choco"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/4/41/Yuzuki_Choco_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/363?cb=20210214143026"))
-
-        student.add(Student("Oozora Subaru"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/c/ce/Oozora_Subaru_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20200516144017"))
-
-        student.add(Student("Ookami Mio"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/4/47/Ookami_Mio_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/500?cb=20210131063047"))
-
-        student.add(Student("Nekomata Okayu"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/6/60/Nekomata_Okayu_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/352?cb=20210131103324"))
-
-        student.add(Student("Inugami Korone"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/0/03/Inugami_Korone_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/378?cb=20210214143108"))
-
-        student.add(Student("Usada Pekora"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/a/a7/Usada_Pekora_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/465?cb=20200825070508"))
-
-        student.add(Student("Uruha Rushia"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/e/ed/Uruha_Rushia_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20200516153214"))
-
-        student.add(Student("Shiranui Flare"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/6/60/Shiranui_Flare_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20200516151223"))
-
-        student.add(Student("Shirogane Noel"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/7/7b/Shirogane_Noel_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/1000?cb=20210315012926"))
-
-        student.add(Student("Houshou Marine"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/9/92/Houshou_Marine_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/512?cb=20200825070426"))
-
-        student.add(Student("Amane Kanata"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/7/75/Amane_Kanata_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20200516155224"))
-
-        student.add(Student("Kiryu Coco"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/e/e5/Kiryu_Coco_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/512?cb=20210428205715"))
-
-        student.add(Student("Tsunomaki Watame"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/5/5f/Tsunomaki_Watame_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/400?cb=20210502070328"))
-
-        student.add(Student("Tokoyami Towa"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/6/64/Tokoyami_Towa_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20200516155509"))
-
-        student.add(Student("Himemori Luna"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/7/78/Himemori_Luna_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20210224092717"))
-
-        student.add(Student("Yukihana Lamy"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/2/2e/Yukihana_Lamy_-_Profile_Picture.png/revision/latest/scale-to-width-down/400?cb=20210202183936"))
-
-        student.add(Student("Momosuzu Nene"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/c/c0/Momosuzu_Nene_-_Profile_Picture.png/revision/latest/scale-to-width-down/1000?cb=20210211194504"))
-
-        student.add(Student("Shishiro Botan"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/a/ae/Shishiro_Botan_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/512?cb=20210212174643"))
-
-        student.add(Student("Omaru Polka"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/3/36/Omaru_Polka_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/800?cb=20210213032500"))
-
-        student.add(Student("Ayunda Risu"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/a/ae/Ayunda_Risu_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/832?cb=20201221005751"))
-
-        student.add(Student("Moona Hoshinova"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/f/ff/Moona_Hoshinova_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/392?cb=20200825072526"))
-
-        student.add(Student("Airani Iofifteen"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/a/a3/Airani_Iofifteen_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/511?cb=20200825070402"))
-
-        student.add(Student("Kureiji Ollie"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/5/50/Kureiji_Ollie_-_Profile_Picture.png/revision/latest/scale-to-width-down/400?cb=20210202234906"))
-
-        student.add(Student("Anya Melfissa"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/a/a9/Anya_Melfissa_-_Profile_Picture.png/revision/latest/scale-to-width-down/400?cb=20210202234950"))
-
-        student.add(Student("Pavolia Reine"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/9/96/Pavolia_Reine_-_Profile_Picture.png/revision/latest/scale-to-width-down/400?cb=20210202235102"))
-
-        student.add(Student("Mori Calliope"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/3/30/Mori_Calliope_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/465?cb=20210214142802"))
-
-        student.add(Student("Takanashi Kiara"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/a/a5/Takanashi_Kiara_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/465?cb=20210214142735"))
-
-        student.add(Student("Ninomae Ina'nis"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/5/59/Ninomae_Inanis_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/465?cb=20210214142831"))
-
-        student.add(Student("Gawr Gura"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/6/6a/Gawr_Gura_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/465?cb=20210214142856"))
-
-        student.add(Student("Watson Amelia"
-            ,"https://static.wikia.nocookie.net/virtualyoutuber/images/c/c5/Watson_Amelia_-_Profile_Picture.jpg/revision/latest/scale-to-width-down/465?cb=20210214142915"))
+        student.add(
+            Student("Student 10",
+                "https://dprd.lumajangkab.go.id/uploads/foto_anggota/foto_anggota_200206041459.jpeg")
+        )
 
         return student
     }
+
+
 }
