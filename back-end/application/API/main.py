64701442012e575f12ce18445e18b082fe4491f1
fachryfathurahman <fachry123fathurahman@gmail.com>
2021-05-24 11:29:00 +0700

    upload file to bucket and make it public

--- back-end/application/API/main.py
+++ back-end/application/API/main.py
@@ -1,13 +1,17 @@
 import os
-
+import hashlib
+import time
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 from google.cloud import storage
+
 app = FastAPI()
 
-CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+CLOUD_STORAGE_BUCKET = os.environ.get('CLOUD_STORAGE_BUCKET')
+
+
 class Profile(BaseModel):
     name: str
     status: Optional[str] = None
@@ -35,12 +39,17 @@ async def create_upload_file(file: UploadFile = File(...)):
     # Get the bucket that the file will be uploaded to.
     bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
 
+    # create name file for encrypted
+    name_file = file.filename + str(time.time())
+    result = hashlib.md5(name_file.encode('utf-8')).hexdigest()
     # Create a new blob and upload the file's content.
-    blob = bucket.blob(file.filename)
+    blob = bucket.blob(str(result))
 
+    content = await file.read()
     blob.upload_from_string(
-        file.read(),
+        content,
         content_type=file.content_type
     )
+    blob.make_public()
     # The public URL can be used to directly access the uploaded file via HTTP.
-    return blob.public_url
+    return {"status": 200, "data": blob.public_url}
