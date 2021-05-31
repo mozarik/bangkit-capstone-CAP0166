61d1a460ab41fbf6ef5d732cdce9423b717b0f8a
fachryfathurahman <fachry123fathurahman@gmail.com>
2021-05-31 12:55:57 +0700

    Update database.py

--- back-end/application/API/app/database.py
+++ back-end/application/API/app/database.py
@@ -1,9 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 ''' DATABASE CONNECTION '''
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/db_watcher"
+# SQLALCHEMY_DATABASE_URL = os.environ.get('DB_CONN')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
