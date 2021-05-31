cd493683d8980da0552bcdb0e2604fdfe49ed524
fachryfathurahman <fachry123fathurahman@gmail.com>
2021-05-31 13:22:33 +0700

    update connection

--- back-end/application/API/app/database.py
+++ back-end/application/API/app/database.py
@@ -1,5 +1,6 @@
 import os
 
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -8,7 +9,33 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/db_watcher"
 # SQLALCHEMY_DATABASE_URL = os.environ.get('DB_CONN')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# db_user = os.environ["DB_USER"]
+# db_pass = os.environ["DB_PASS"]
+# db_name = os.environ["DB_NAME"]
+# db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+# cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+db_user = "root"
+db_pass = "1234567890"
+db_name = "db_watcher"
+db_socket_dir = "/cloudsql"
+cloud_sql_connection_name = "imperial-berm-311408:asia-southeast2:the-watchersql"
+
+engine = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+    sqlalchemy.engine.url.URL.create(
+        drivername="mysql+pymysql",
+        username=db_user,  # e.g. "my-database-user"
+        password=db_pass,  # e.g. "my-database-password"
+        database=db_name,  # e.g. "my-database-name"
+        query={
+            "unix_socket": "{}/{}".format(
+                db_socket_dir,  # e.g. "/cloudsql"
+                cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+        }
+    ),
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
