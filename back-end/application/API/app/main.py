519f2be665bf5eb6126e21c100dd68ecef70018b
fachryfathurahman <fachry123fathurahman@gmail.com>
2021-06-01 13:11:58 +0700

    Update main.py

--- back-end/application/API/app/main.py
+++ back-end/application/API/app/main.py
@@ -32,28 +32,28 @@ def read_root():
     return {"Hello": "World"}
 
 
-# @app.post("/uploadfile/")
-# async def create_upload_file(file: UploadFile = File(...)):
-#     # Create a Cloud Storage client.
-#     gcs = storage.Client()
-#
-#     # Get the bucket that the file will be uploaded to.
-#     bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-#
-#     # create name file for encrypted
-#     name_file = file.filename + str(time.time())
-#     result = hashlib.md5(name_file.encode('utf-8')).hexdigest()
-#     # Create a new blob and upload the file's content.
-#     blob = bucket.blob(str(result))
-#
-#     content = await file.read()
-#     blob.upload_from_string(
-#         content,
-#         content_type=file.content_type
-#     )
-#     blob.make_public()
-#     # The public URL can be used to directly access the uploaded file via HTTP.
-#     return {"status": 200, "data": blob.public_url}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # create name file for encrypted
+    name_file = file.filename + str(time.time())
+    result = hashlib.md5(name_file.encode('utf-8')).hexdigest()
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob(str(result))
+
+    content = await file.read()
+    blob.upload_from_string(
+        content,
+        content_type=file.content_type
+    )
+    blob.make_public()
+    # The public URL can be used to directly access the uploaded file via HTTP.
+    return {"status": 200, "data": blob.public_url}
 
 
 @app.post("/add-preprocess/", response_model=schemas.Preprocess)
