1f441039cce52233e502de0dcd32805260444aa9
fachryfathurahman <fachry123fathurahman@gmail.com>
2021-06-01 17:25:24 +0700

    Update entry_ml.py

--- back-end/application/API/app/entry_ml.py
+++ back-end/application/API/app/entry_ml.py
@@ -1,7 +1,9 @@
 import numpy as np
 from cv2 import cv2
 
+
 def numpyarray_to_blob(numpyArray: np):
+    numpyArray = cv2.cvtColor(numpyArray, cv2.COLOR_BGR2RGB)
     success, encoded_image = cv2.imencode('.jpg', numpyArray)
     blob_file = encoded_image.tobytes()
     return blob_file
