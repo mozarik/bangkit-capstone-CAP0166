940195160c4e5653c09c12863a94877487273068
fachryfathurahman <fachry123fathurahman@gmail.com>
2021-06-04 20:25:47 +0700

    update website

--- front-end/Web Version/dashboard.py
+++ front-end/Web Version/dashboard.py
@@ -1,13 +1,16 @@
+from decimal import Decimal
+
 from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
-from flask_mysqldb import MySQL,MySQLdb
+from flask_mysqldb import MySQL, MySQLdb
 from werkzeug.utils import secure_filename
 import urllib.request
 import os
+import requests
 from werkzeug.utils import secure_filename
 from datetime import datetime
+import json
 
 app = Flask(__name__)
-
 
 app.secret_key = "caircocoders-ednalan"
 #
@@ -17,48 +20,53 @@ app.secret_key = "caircocoders-ednalan"
 # app.config['MYSQL_DB'] = 'thewatcher'
 # app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # mysql = MySQL(app)
- 
+#
 # app.config['UPLOAD_FOLDER'] = 'static/uploads'
-   
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-   
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
 def allowed_file(filename):
- return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+
 @app.route("/uploadfile")
 def index():
     return render_template('uploadfile.html', menu='dashboard', submenu='upload')
 
-# @app.route("/showdata")
-# def index2():
-#     cur = mysql.connection.cursor()
-#     cur.execute("SELECT * FROM uploads")
-#     uploads = cur.fetchall()
-#     cur.close()
-#     return render_template('showdata.html', menu='dashboard', submenu='show', data=uploads)
 
-@app.route("/upload",methods=["POST","GET"])
+@app.route("/showdata")
+def index2():
+    # cur = mysql.connection.cursor()
+    # cur.execute("SELECT * FROM uploads")
+    # uploads = cur.fetchall()
+    # cur.close()
+
+    url = "https://api-watcher-n762eur5da-et.a.run.app/postprocess/"
+    r = requests.get(url)
+    data = r.json()["data"]
+    for i in data:
+        i['percentage'] = str(round((1 - Decimal(i['percentage'])) *100)) + "%"
+    return render_template('showdata.html', menu='dashboard', submenu='show', data=data)
+
+
+@app.route("/upload", methods=["POST", "GET"])
 def upload():
     file = request.files['uploadFile']
-    filename = secure_filename(file.filename)
-    
-    if file and allowed_file(file.filename):
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        filenameimage = file.filename
 
-        # today = datetime.today()
-        # cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # cur.execute("INSERT INTO uploads (file_name,upload_time) VALUES (%s,%s)",[filenameimage,today])
-        # mysql.connection.commit()
-        # cur.close()
-        msg  = 'File successfully uploaded ' + file.filename + ' to the database!'
-    else:
-        msg  = 'Invalid Uplaod only png, jpg, jpeg, gif'
-    return jsonify({'htmlresponse': render_template('response.html', msg=msg, filenameimage=filenameimage)})
+    url = "https://api-watcher-n762eur5da-et.a.run.app/uploadfile/"
+    files = {'file': file}
+    r = requests.post(url, files=files)
+    data = r.json()["data"]
+    img_url = data["img_url"]
+
+    return jsonify({'htmlresponse': render_template('response.html', msg="", filenameimage=img_url)})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
